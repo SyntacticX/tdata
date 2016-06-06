7adb7530f0d666ae3c2849fb798f801ae2d317a8
Ryan <rmm54@njit.edu>
2016-06-06 17:37:25 -0400

    Testing file i/o and created function

--- tdata/Source.cpp
+++ tdata/Source.cpp
@@ -5,22 +5,53 @@
 
 using namespace std;
 
+void createInputFile(ifstream &inFile, ofstream &outFile)
+{
+	string type;
+	string line;
+	while (inputFile >> type)
+	{
+		getline(inputFile, line);
+		if (type == "char")
+		{
+			cout << "This is a char: ";
+		}
+		if (type == "int")
+		{
+			cout << "This is an int: ";
+		}
+		if (type == "int[]")
+		{
+			cout << "This is an int array: ";
+		}
+		cout << line << endl;
+	}
+
+	// outputFile << x << endl;
+}
+
 int main()
 {
-	string s;
+	string type;
+	string line;
 	ifstream dataFile;
 	dataFile.open("example.tdata", ios::in | ios::beg);
 
-	/*
 	ofstream inputFile;
 	inputFile.open("temp.tin", ios::out | ios::trunc);
-	*/
 
-	if
-	while (dataFile >> s)
+	if (dataFile.is_open())
 	{
-		cout << s << endl;
+		createInputFile(dataFile, inputFile);
 	}
+	else
+	{
+		cout << "Specified data file does not exist. Please recheck specified." << endl;
+		return 1;
+	}
+		
+	dataFile.close();
+	inputFile.close();
 
 
 	return 0;
