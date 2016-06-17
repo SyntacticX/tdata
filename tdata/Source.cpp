90b9557560c6a7b44efefb861cb593ad9ba24d4e
Ryan <rmm54@njit.edu>
2016-06-17 16:19:41 -0400

    Did a lot. Overall: Added Cassie's extractFile and removeComments functionality. Inside createInputFile function: Reworked function to take .tdata file path and return .tin file path. Added logic to create temporary data structure to better parse the data after file extraction. Added logic to convert int data. Added logiv to convert char data.

--- tdata/Source.cpp
+++ tdata/Source.cpp
@@ -1,58 +1,142 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
-void createInputFile(ifstream &inFile, ofstream &outFile)
-{
-	string type;
+string removeComments(string line){
+	/* If comment is found (whole line), skip line
+	* If comment is found (in line), delete comment out.
+	* Otherwise, return the line
+	*/
+
+	size_t found = line.find("#");
+	if (found != string::npos)
+		if (found == 0)
+			return "";
+		else
+			return line.substr(0, found);
+	else
+		return line;
+}
+
+vector<string> extractFile(string filePath){
+	ifstream file;
 	string line;
-	while (inputFile >> type)
-	{
-		getline(inputFile, line);
-		if (type == "char")
-		{
-			cout << "This is a char: ";
+	vector<string> fileLines(0);
+
+	file.open(filePath);
+
+	// Traverse through each line, checks for comments and removes if found
+	if (file.is_open()) {
+		cout << "file " << filePath << " is open\n" << endl;
+		while (getline(file, line)) {
+			line = removeComments(line);
+			if (!line.empty()){
+				fileLines.push_back(line);
+			}
 		}
-		if (type == "int")
-		{
-			cout << "This is an int: ";
-		}
-		if (type == "int[]")
-		{
-			cout << "This is an int array: ";
-		}
-		cout << line << endl;
+		file.close();
+		cout << "file " << filePath << " is closed, extraction performed\n" << endl;
+	}
+	else {
+		cout << "unable to open file " << filePath << "\n" << endl;
 	}
 
-	// outputFile << x << endl;
+	return fileLines;
 }
+
+
+string stringTrans(string str){
+	int numeric;
+	string translated;
+	for (int i = 1; i < str.length()-1; i++){ // start at 1 and end at length-1 to discard ' ' or " "
+		if (i > 1){
+			translated += " "; // if not the first letter of the string, append space before translation
+		}
+		numeric = (int) str.at(i);
+		translated += to_string(numeric);
+	}
+	return translated;
+}
+
+void createInputFile(string inFile)
+{
+	vector<string> dataLines = extractFile(inFile); // extract .tdata file in a vector containing all the file lines (comments removed)
+	int size = dataLines.size();
+	string type;
+	string name;
+
+
+	for (int i = 0; i < size; i++){ // iterate through file lines, create a vector containing all sub strings
+		vector<string> line;
+		istringstream iss(dataLines[i]);
+		string inputLine;
+
+		while (iss){
+			string subString;
+			iss >> subString;
+			line.push_back(subString);
+		}
+
+		if (line[2] == ":="){ // check to make sure ":=" is at the correct position, else throw error
+			
+			type = line[0];
+			name = line[1];
+
+			if (type == "int"){
+				inputLine = name; // variable name
+				inputLine += " := "; // equals
+				inputLine += "["; // opening bracket
+				inputLine += line[3]; // value
+				inputLine += "]"; // closing bracket
+			}
+			else if (type == "char"){
+				inputLine = name; // variable name
+				inputLine += " := "; // equals
+				inputLine += "["; // opening bracket
+				inputLine += stringTrans(line[3]); // value
+				inputLine += "]"; // closing bracket
+
+			}
+
+		}
+		else {
+			cout << "Error on line " << i + 1 << ": Variable declarations must be of format \"<type> <name> := <value>\"" << endl;
+			break;
+		}
+
+		cout << inputLine << endl;
+	
+	}
+
+
+}
+
 
 int main()
 {
 	string type;
 	string line;
-	ifstream dataFile;
-	dataFile.open("example.tdata", ios::in | ios::beg);
+	string dataFile = "example.tdata";
 
-	ofstream inputFile;
-	inputFile.open("temp.tin", ios::out | ios::trunc);
+	createInputFile(dataFile);
+	/*
+	string s = "a";
+	// char x = a.at(0);
+	int i = (int) s.at(0);
 
-	if (dataFile.is_open())
-	{
-		createInputFile(dataFile, inputFile);
-	}
-	else
-	{
-		cout << "Specified data file does not exist. Please recheck specified." << endl;
-		return 1;
-	}
-		
-	dataFile.close();
-	inputFile.close();
+	string g;
 
+	g += s;
+	g += " ";
+	g += to_string(i);
 
+	cout << g << endl;
+	*/
 	return 0;
 }
